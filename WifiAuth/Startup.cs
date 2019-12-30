e2c4e618d05db1307385b66ba56f2c61c2231b2c
Robert Scullin <rscullin@users.noreply.github.com>
2019-12-29 23:55:13 -0500

    Adding static file serving for eventual web frontend

--- WifiAuth/Startup.cs
+++ WifiAuth/Startup.cs
@@ -15,60 +15,63 @@ using Newtonsoft.Json;
 
 namespace WifiAuth
 {
-      public class Startup
-      {
+    public class Startup
+    {
 
-            // We'll retreive these from the Secrets Manager
-            // See the Readme on how to set these
-            public static string _laptopPassword { get; private set; }
-            public static string _APIKey { get; private set; }
-            public static string _UberAPIAddress { get; private set; }
+        // We'll retreive these from the Secrets Manager
+        // See the Readme on how to set these
+        public static string _laptopPassword { get; private set; }
+        public static string _APIKey { get; private set; }
+        public static string _UberAPIAddress { get; private set; }
 
-            public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMemoryCache();
+            services.AddMvc().AddNewtonsoftJson(options =>
             {
-                  Configuration = configuration;
+                options.SerializerSettings.Formatting = Formatting.Indented;
+            });
+            services.AddControllers();
+
+            services.AddDbContext<WifiAuthContext>(options =>
+                                                  options.UseSqlite("Data Source=wifiauth.db"));
+
+            // Load settings from the Secret Manager
+            // TODO: This needs to bail if not set, rather than silently continuing and then erroring later
+            _laptopPassword = Configuration["LaptopPassword"];
+            _APIKey = Configuration["APIKey"];
+            _UberAPIAddress = Configuration["UberServer"];
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, WifiAuthContext context)
+        {
+
+            // Auto create/migrate the db on start
+            context.Database.Migrate();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
             }
 
-            public IConfiguration Configuration { get; }
-
-            // This method gets called by the runtime. Use this method to add services to the container.
-            public void ConfigureServices(IServiceCollection services)
+            //app.UseMvc();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
             {
-                  services.AddMemoryCache();
-                  services.AddMvc().AddNewtonsoftJson(options =>
-                  {
-                      options.SerializerSettings.Formatting = Formatting.Indented;
-                  });
-                  services.AddControllers();
-
-                   services.AddDbContext<WifiAuthContext>(options =>
-                                                         options.UseSqlite("Data Source=wifiauth.db"));
-
-                  // Load settings from the Secret Manager
-                  // TODO: This needs to bail if not set, rather than silently continuing and then erroring later
-                  _laptopPassword = Configuration["LaptopPassword"];
-                  _APIKey = Configuration["APIKey"];
-                  _UberAPIAddress = Configuration["UberServer"];
-
-            }
-
-            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-            public void Configure(IApplicationBuilder app, IWebHostEnvironment env, WifiAuthContext context)
-            {
-
-                // Auto create/migrate the db on start
-                context.Database.Migrate();
-
-                if (env.IsDevelopment())
-                  {
-                        app.UseDeveloperExceptionPage();
-                  }
-
-                  //app.UseMvc();
-                  app.UseRouting();
-                  app.UseEndpoints(endpoints => {
-                        endpoints.MapControllers();
-                  });
-            }
-      }
+                endpoints.MapControllers();
+            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
 }
